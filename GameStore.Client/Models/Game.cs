b1ca94e4cc84a4f8aa003a05cfb2d539f8996b42
brianackley001 <brian.ch.ackley@gmail.com>
2023-11-09 21:44:39 -0800

    form validation

--- GameStore.Client/Models/Game.cs
+++ GameStore.Client/Models/Game.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameStore.Client.Models;
 
 public class Game
 {
     public int ID { get; set; }
+    [Required]
+    [StringLength(50, MinimumLength = 3)]
     public required string Name { get; set; }
+    [Required]
+    [StringLength(20, MinimumLength = 3)]
     public required string Genre { get; set; }
+    [Range(1.00, 99.99)]
     public decimal Price { get; set; }
     public DateTime ReleaseDate { get; set; }
 }
